4a82c7845d2c727016bc4c58f5edaed93df89ce9
_asdx_ <xasdx@tuta.io>
2017-12-18 17:27:28 +0000

    Add a few test cases

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 import test from "ava"
 import _ from "."
 
-test("passes", t => t.pass())
+test("merges two objects", t => t.deepEqual(_([{ a: 0 }, { b: true }]), { a: 0, b: true }))
+
+test("handles one object", t => t.deepEqual(_([{ s: "hello" }]), { s: "hello" }))
+
+test("handles an empty array", t => t.deepEqual(_([]), {}))
