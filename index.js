3806250afcdbd488ab50a0cb66fbd92b283046b6
_asdx_ <xasdx@tuta.io>
2017-12-19 17:43:47 +0000

    Implement core functionality

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-module.exports = () => {}
+let mergeObjects = (objects) => {
+  let result = cloneObject(objects[0])
+  for (let i = 1; i < objects.length; i += 1) {
+    iterateOwnProperties(objects[i], (property, object) => {
+      result[property] = object[property]
+    })
+  }
+  return result
+}
+
+let cloneObject = (object) => {
+  let result = {}
+  iterateOwnProperties(object, (property) => {
+    result[property] = object[property]
+  })
+  return result
+}
+
+let iterateOwnProperties = (object, f) => {
+  for (let property in object) {
+    if (object.hasOwnProperty(property)) {
+      f(property, object)
+    }
+  }
+}
+
+module.exports = mergeObjects
