06ffc5c6c9f2391bbe928920e3bd772fdcfe20c1
_asdx_ <xasdx@tuta.io>
2017-12-19 17:35:12 +0000

    Add additional test cases

--- test.js
+++ test.js
@@ -6,3 +6,11 @@ test("merges two objects", t => t.deepEqual(_([{ a: 0 }, { b: true }]), { a: 0, 
 test("handles one object", t => t.deepEqual(_([{ s: "hello" }]), { s: "hello" }))
 
 test("handles an empty array", t => t.deepEqual(_([]), {}))
+
+test("handles mixed objects and non-objects in the array", t => t.deepEqual(_([{ a: 0 }, "", true, () => {}]), { a: 0 }))
+
+test("handles anything that is not an array", t => t.deepEqual(_(""), {}))
+
+test("does not merge recursively", t => t.deepEqual(_([{ a: { b: 0 } }, { a: { c: "d" } }]), { a: { c: "d" } }))
+
+test("overwrites existing properties", t => t.deepEqual(_([{ a: 0, b: "1" }, { b: 1 }]), { a: 0, b: 1 }))
